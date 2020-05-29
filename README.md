ff5717b4c55209b11e2021d42aab1168ed390b6d
Fasika Assegei <fasika.assegei@nnip.com>
2020-05-29 14:18:03 +0200

    Initialize project, commit for Amplify

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is part of a workshop on Amplify. 
+
+TODO Add more content
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+import {withAuthenticator} from 'aws-amplify-react';
+import {Header} from 'semantic-ui-react';
+
+Amplify.configure(aws_exports);
+
+function App() {
+  return (
+        <Header as="H1">
+          Hello World
+        </Header>
+  );
+}
+
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  signUpConfig: {
+    hiddenDefaults: ['phone_number']
+  }
+});
