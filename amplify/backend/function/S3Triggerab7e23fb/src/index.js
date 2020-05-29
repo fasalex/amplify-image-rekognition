b3ecf46165b1fa218b7657a482f185b6d5d2988d
Fasika Assegei <fasika.assegei@nnip.com>
2020-05-29 16:11:56 +0200

    Added upload functionality for the backend, initialized the storage.

---
+++ amplify/backend/function/S3Triggerab7e23fb/src/index.js
@@ -0,0 +1,14 @@
+/* Amplify Params - DO NOT EDIT
+	API_PHOTOALBUMS_GRAPHQLAPIENDPOINTOUTPUT
+	API_PHOTOALBUMS_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */// eslint-disable-next-line
+exports.handler = function(event, context) {
+  console.log('Received S3 event:', JSON.stringify(event, null, 2));
+  // Get the object from the event and show its content type
+  const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
+  const key = event.Records[0].s3.object.key; //eslint-disable-line
+  console.log(`Bucket: ${bucket}`, `Key: ${key}`);
+  context.done(null, 'Successfully processed S3 event'); // SUCCESS with message
+};
